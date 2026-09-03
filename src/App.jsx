13e223cd976d074831716ffdd4808902298c9900
dija-hub <dija5408@gmail.com>
2026-09-03 21:00:28 +0500

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
-import { useState } from "react";
+import "./App.css";
 import Web from "./Web";
-import Dashboard from "./dashboard";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showDashboard, setShowDashboard] = useState(false);
-
-  if (showDashboard && isLoggedIn) {
-    return (
-      <Dashboard
-        setIsLoggedIn={setIsLoggedIn}
-        setShowDashboard={setShowDashboard}
-      />
-    );
-  }
-
-  return (
-    <Web
-      setIsLoggedIn={setIsLoggedIn}
-      setShowDashboard={setShowDashboard}
-    />
-  );
+  return <Web />;
 }
 
 export default App;
