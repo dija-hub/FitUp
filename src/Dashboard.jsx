836928cad2187f28c5295080e8255ea05c997f4d
dija-hub <dija5408@gmail.com>
2026-09-09 20:50:57 +0500

    tracker

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -14,7 +14,7 @@ import {
 } from "lucide-react";
 
 import { supabase } from "./utils/supabase";
-import DashboardNav from "./DashboardNav";
+import DashboardNav from "./Dashboardnav";
 import "./Dashboard.css";
 
 function Dashboard({
